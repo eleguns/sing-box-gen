5a3fd020cbc604ab740a301f077856d24fcaf060
eleguns <d88560771@gmail.com>
2023-01-20 10:46:23 -0800

    new commit

--- sb-gen.py
+++ sb-gen.py
@@ -1,5 +1,5 @@
 import os
-
+from uuid import uuid4
 
 class SingBoxGen :
     def __init__(self) -> None:
@@ -28,8 +28,36 @@ class SingBoxGen :
             self.vless_ws()
         if config == 2:
             self.vless_ws_tls()
-
-
+    def get_port(self):
+        port = int(input('Enter port number between 1-60000, default: 443'))
+        if port not in range(1, 60001):
+            return self.get_port()
+        return port
+    def get_dns_server(self):
+        tag_list = ['google-dns', 'cloudflare-dns']
+        address_list = ['8.8.8.8', '1.1.1.1']
+        dns_server = int(input('\n1) google dns\n2) cloudflare dns\n\nEnter number of dns server you want'))
+        if dns_server not in [1,2]:
+            return self.get_dns_server()
+        return tag_list[dns_server - 1], address_list[dns_server - 1]
+    def ask_cert(self):
+        auto_cert = input('do you have certificate or you want from script to create that (m: manual, a: automatic):')
+        if auto_cert not in ['m', 'a']:
+            return self.ask_cert()
+    def vmess_ws_tls(self):
+        port = self.get_port()
+        uuid = uuid4()
+        alterId = input('Enter number of alterId, default: 0')
+        dns-tag, dns-address = self.get_dns_server()
+        auto_cert = self.ask_cert()
+        if auto_cert == 'm':
+            cert_path = input('enter your cert path:')
+            key_path  = input('enter your key path:')
+        if auto_cert == 'a':
+            sni = input('enter your url address for sni, like: www.google.com')
+            if sni == '':
+                sni = 'www.ray.uk'
+        
 
 
 def main():
